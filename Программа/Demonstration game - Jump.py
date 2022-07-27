b1974253a23f545379710ce90db4807f681b3dba
Mayya <mayya.mukhina@icloud.com>
2022-07-27 16:20:56 +0300

    files

--- Программа/Demonstration game - Jump.py
+++ Программа/Demonstration game - Jump.py
@@ -1,82 +1,123 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# vim:fileencoding=utf-8
 import sys
 import random
 import pygame
+from pygame.locals import *
 import math
-print(" ")
-print("  *****  Демонстрационная Игра  *****  ")
-print(" ")
-question = raw_input('Напишите да, чтобы начать игру? ')
-print(" ")
-cancel = "run"
-while 1:   
-    if question != "да":
-        answer = raw_input("Вы уверены, что хотите выйти? ")
-        print(" ")
-        if answer == "да ":
-            pygame.quit()
-        else:
-            question = "да"
-            answer = "нет"
-     
+
+class Game():
+    print(" ")
+    print("  *****  Демонстрационная Игра  *****  ")
+    print(" ")
+    state = True
+    
     pygame.init()
-    width = 700;
-    height = 700
+    width = 800
+    height = 800
     pygame.display.set_caption("Демонстрационная Игра")
     screen = pygame.display.set_mode((width, height))
-    background = pygame.display.set_mode()
-    blue_square = pygame.image.load("player.bmp").convert_alpha(background)
-    x1 = 0;
-    y1 = 620;
-    
-    screen.fill((255,255,255))
-    screen.blit(blue_square,(x1,y1))
+    image_back = pygame.image.load("images/background2.jpg")
+    screen.blit(image_back, (0,0))
+
+    run1 = pygame.image.load("images/run1.png")
+    run2 = pygame.image.load("images/run2.png")
+    run3 = pygame.image.load("images/run3.png")
+    run4 = pygame.image.load("images/run4.png")
+    run5 = pygame.image.load("images/run5.png")
+    run6 = pygame.image.load("images/run6.png")
+    run7 = pygame.image.load("images/run7.png")
+    run8 = pygame.image.load("images/run8.png")
+    run9 = pygame.image.load("images/run9.png")
+    run10 = pygame.image.load("images/run10.png")
+
+    jump = pygame.image.load("images/jump.png")
+
+    image = [run1, run2, run3, run4, run5, run6, run7, run8, run9, run10]
+
     pygame.display.update()
-    print(" ")
-    
-    
-    constant = -0.005
-    constant2 = math.sqrt(60000)
 
-    x2 = constant2 * - 1
-    y2 = 0
+    print("Нажмите на экран, чтоб продолжить или повторить; Enter, чтобы завершить")
 
-    constant3 = (constant2 * 2) / 122
+    while state:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
 
-    stage6 = raw_input("          Этап 6 ")
-    print("Координат x Cинего квадрата = {} " .format(x1))
-    print("Координат у Cинего квадрата = {} " .format(y1))
-    if stage6 == "":
-        while x2 <= round(constant2):
-            square = x2 * x2
-            y2 =  float((constant * square) + 300)
-            
-            x1 = x2 + constant2
-            y1 = 620 - y2
-            print("X = {}          Y = {}" .format(int(x1), int(y1)))
-            screen.fill((255,255,255))
-            screen.blit(blue_square,(x1,y1))
-            pygame.display.update()
-            x2 += constant3
+            if event.type == pygame.MOUSEBUTTONUP:
+                x1 = 0
+                y1 = 600
+                screen.blit(run1,[x1,y1])
+                pygame.display.update()
+                
+                print(" ")
+                
+                constant = -0.0075
+                constant2 = math.sqrt(40000)
 
-        
+                x2 = (constant2 * - 1)
+                y2 = 0
+
+                constant3 = (constant2 * 2) / 100
+                count = 0
+                num = 0
+
+                while x1 != 120:
+                    if num != 9:
+                        num += 1
+                    else:
+                        num = 0
+
+                    x1 += 7.5
+                    screen.blit(image_back,(0,0))
+                    screen.blit(image[num],(x1,y1))
+                    pygame.display.update()
+                    print("X = {}          Y = {}" .format(int(x1), int(y1)))
+                    pygame.time.delay(40)
+                    
+                
+                while x2 <= round(constant2):
+                    square = x2 * x2
+                    y2 =  float((constant * square) + 300)
+                    
+                    x1 = x2 + constant2 + 120
+                    y1 = 600 - y2
+                    print("X = {}          Y = {}" .format(int(x1), int(y1)))
+                    screen.blit(image_back,(0,0))
+                    screen.blit(jump,(x1,y1))
+                    pygame.display.update()
+                    x2 += constant3
+                    pygame.time.delay(5)
 
 
-    
+                while x1 < 640:
+                    if num != 9:
+                        num += 1
+                    else:
+                        num = 0
 
-                          
-    print(" ")
-    print("Вы Победили!!!")
-    print(" ")
-    finalQ = raw_input("Хотите начать все сначала?")
-    print(" ")
-    print(" ")
+                    x1 += 7.5
+                    screen.blit(image_back,(0,0))
+                    screen.blit(image[num],(x1,y1))
+                    pygame.display.update()
+                    print("X = {}          Y = {}" .format(int(x1), int(y1)))
+                    pygame.time.delay(40)
+                
+                
+                screen.blit(image_back,(0,0))
+                screen.blit(run1,(x1,y1))
+                pygame.display.update()
+                
+                print("")
+                print("Нажмите на экран, чтоб продолжить или повторить; Enter, чтобы завершить")
 
-    if finalQ != "да":
-        pygame.quit()
-        sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
 
 
-pygame.quit()
+if __name__ == '__main__':
+    ex = Game()
+
---
+++ Программа/Demonstration game - Locating.py
@@ -0,0 +1,178 @@
+import sys
+import random
+import pygame
+from pygame.locals import *
+import math
+
+class Game():
+    print(" ")
+    print("  *****  Демонстрационная Игра  *****  ")
+    print(" ")
+    state = True
+    
+    pygame.init()
+    width = 700
+    height = 700
+    pygame.display.set_caption("Демонстрационная Игра")
+    screen = pygame.display.set_mode((width, height))
+
+    red_square = pygame.image.load("images/player2.png")
+    blue_square = pygame.image.load("images/player.png")
+    
+    screen.fill((255,255,255))
+    
+    pygame.display.update()
+
+    count = 1
+
+    print("Нажмите на экран, чтоб продолжить или повторить; Enter, чтобы завершить")
+
+    while state:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if count == 1:
+                    x = random.randint(0,620);
+                    y = random.randint(0,620);
+                    print(" ")
+                    print("          Этап 1 ")
+                    print("Координат x Cинего квадрата = {} " .format(x))
+                    print("Координат у Cинего квадрата = {} " .format(y))
+                    print(" ")
+                    x2 = random.randint(0,620);
+                    y2 = random.randint(0,620);
+                    print("Координат x Красного квадрата = {} " .format(x2))
+                    print("Координат у Красного квадрата = {} " .format(y2))
+                    screen.fill((255,255,255))
+                    screen.blit(blue_square,(x,y))
+                    screen.blit(red_square,(x2,y2))
+                    pygame.display.update()
+                    count += 1
+
+                elif count == 2:
+                    print(" ")
+                    print("          Этап 2 ")
+
+                    sidex = abs(x2 - x)
+                    print("Сторона х = {}" .format(sidex))
+
+                    sidey = abs(y2 - y)
+                    print("Сторона у = {}" .format(sidey))
+                   
+                    print(" ")
+                    count += 1
+
+                elif count == 3:
+                    print("          Этап 3 ")
+                    hypsqt = (sidex * sidex) + (sidey * sidey )
+                    hyp = math.sqrt(hypsqt)
+                    print("Гипотенуза = {}" .format(hyp))
+                    print(" ")
+                    count += 1
+
+                elif count == 4:
+                    print("          Этап 4 ")
+                    
+                    if y2 > y:
+                        if x2 > x:
+                            preangle = float(sidey) / float(sidex)
+                            angle = math.degrees(math.atan(preangle))
+                            bearing = 270 + angle
+                        elif x2 < x:
+                            preangle = float(sidey) / float(sidex)
+                            angle = math.degrees(math.atan(preangle))
+                            bearing = 90 - angle
+                        else:
+                            angle = 0
+                            bearing = 0
+                    elif y2 < y:
+                        if x2 > x:
+                            preangle = float(sidex) / float(sidey)
+                            angle = math.degrees(math.atan(preangle))
+                            bearing = 180 + angle
+                        elif x2 < x:
+                            preangle = float(sidex) / float(sidey)
+                            angle = math.degrees(math.atan(preangle))
+                            bearing = 90 + (90 - angle)
+                        else:
+                            angle = 0
+                            bearing = 180
+                    else:
+                        if x2 > x:
+                            angle = 0
+                            bearing = 270
+                        elif x2 < x:
+                            angle = 0
+                            bearing = 90
+                        else:
+                            angle = 0
+                            bearing = 0
+                                
+                    print("Угол = {}" .format(angle))
+                    print("Пеленг = {}" .format(bearing))
+                    print(" ")
+                    count += 1
+
+            elif count == 5:
+                print("          Этап 5 ")
+
+                if sidex != 0:
+                    gradient = float(sidey) / float(sidex)
+                else:
+                    gradient = 0
+                
+                print("Градиент = {}" .format(gradient))
+                print(" ")
+                step = 0
+                
+                while (round(x2) != round(x)) or (round(y2) != round(y)):
+                    if x2 > x:
+                        step = 1
+                        if y2 > y:
+                            x2 -= 1
+                            y2 -= gradient
+                        elif y2 < y:
+                            x2 -= 1
+                            y2 += gradient
+                        else:
+                            x2 -= 1
+                    elif x2 < x:
+                        step = 1
+                        if y2 >= y:
+                            x2 += 1
+                            y2 -= gradient
+                        elif y2 <= y:
+                            x2 += 1
+                            y2 += gradient
+                        else:
+                            x2 += 1
+
+                    elif step == 0:
+                        if x2 == x:
+                            if y2 > y:
+                                y2 -= 1
+                            elif y2 < y:
+                                y2 += 1
+
+                    screen.fill((255,255,255))
+                    screen.blit(blue_square, (x,y))
+                    screen.blit(red_square, (x2,y2))
+                    pygame.display.update()
+                    print("X = {}          Y = {}" .format(x2, int(y2)))
+
+                    count = 1
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    ex = Game()
+
